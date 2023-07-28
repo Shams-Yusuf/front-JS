ebb09aea81a5be0bffde26a601f5f74c35c77a25
shams yusuf <shamsudinvu@gmail.com>
2023-07-28 14:53:47 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,36 +1,22 @@
-let name = prompt("Muhammad");
-let address = prompt("Grozny");
-let phone = prompt("89326826923");
-let date = new Date("1999.02.28");
-const COLOR_GREEN = "00ff00";
-const BLOCK_SIZE = 35;
-const box = {
-  product: "товары",
-  prices: "цены",
-};
-box.prices = 137;
+// let name = prompt("Adam");
+// let address = prompt("Grozny");
+// let phone = prompt("89326826923");
+let name = "Adam";
+let address = "Grozny";
+let phone = "89326826923";
+
+let data = "28.02.1999";
+
+const product = "товары";
+// product = "цены"; ошибка
+
 Math.PI;
 
-console.log(name + "Muhammad" + address + "Grozny" + phone + "89326826923");
-const valueInputNode = document.querySelector(".js-value-input");
-const currencySelectorNode = document.querySelector(".js-currency-selector");
-const outputNode = document.querySelector(".js-output");
-valueInputNode.addEventListener("input", function () {
-  const rub = Number(valueInputNode.value);
-  const currency = currencySelectorNode.value;
-  const result = convert(rub, currency);
-  console.log(result);
-  outputNode.innerText = result;
-});
-currencySelectorNode.addEventListener("change", function () {
-  const rub = Number(valueInputNode.value);
-  const currency = currencySelectorNode.value;
-  const result = convert(rub, currency);
-  console.log(result);
-  outputNode.innerText = result;
-});
+let $ = 2000;
+let rub = 90;
+
+console.log($ / rub);
+console.log(`студент ${name}, адрес ${address}, номер ${phone}`);
 console.log(Math.PI);
-console.log(date);
-console.log(box);
-console.log(COLOR_GREEN);
-console.log(BLOCK_SIZE);
+console.log(data);
+console.log(product);
